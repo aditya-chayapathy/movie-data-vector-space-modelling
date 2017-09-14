8b3888f8e7e23f5f933c718f1824a2b2f688e146
Aditya <aditya-may9@users.noreply.github.com>
2017-09-13 21:22:15 -0700

    skeleton for task 2

---
+++ Phase 1/sample/genre_vector.py
@@ -0,0 +1,156 @@
+import logging
+
+import pandas as pd
+
+import config
+import extractor
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+conf = config.ParseConfig()
+
+
+class GenreTag(object):
+    def __init__(self):
+        self.data_set_location = conf.config_section_mapper("filePath").get("data_set_location")
+        self.extract_data = extractor.ExtractData(self.data_set_location)
+
+    def get_mlmovies_data(self):
+        mlmovies = self.extract_data.data_extractor("mlmovies.csv")
+        return mlmovies
+
+    def get_mltags_data(self):
+        mltags = self.extract_data.data_extractor("mltags.csv")
+        return mltags
+
+    def get_genome_tags_data(self):
+        genome_tags = self.extract_data.data_extractor("genome-tags.csv")
+        return genome_tags
+
+    def get_combined_data(self):
+        mltags = self.get_mltags_data()
+        genome_tags = self.get_genome_tags_data()
+        mlmovies = self.get_mlmovies_data()
+
+        temp = mltags.merge(genome_tags, left_on="tagid", right_on="tagId", how="left")
+        del temp['tagId']
+
+        result = temp.merge(mlmovies, on="movieid", how="left")
+        del result['userid']
+        del result['moviename']
+        del result['tagid']
+
+        return result
+
+    def get_combined_data_for_genre(self, genre):
+        genre_data = self.get_combined_data()
+        result = genre_data[genre_data['genres'].str.contains(genre)]
+        return result
+
+    def get_weighted_tags_for_genre_and_model(self, genre, model):
+        genre_data = self.get_combined_data_for_genre(genre)
+        for index, row in genre_data.iterrows():
+            movie_id = row['movieid']
+            tag = row['tag']
+            timestamp = row['timestamp']
+            row_weight = self.get_timestamp_value(timestamp) + self.get_model_value(genre, movie_id, tag, model)
+            genre_data['row_weight'] = pd.Series(row_weight, index=genre_data.index)
+
+        tag_group = genre_data.groupby(['tag'])
+        result = {}
+        for tag, df in tag_group:
+            result[tag] = sum(df['row_weight'])
+
+        return result
+
+        # def get_model_value(self, actor_id, movie_id, tag_of_movie, model):
+        #     if model == "tf":
+        #         return self.get_tf_value(actor_id, movie_id, tag_of_movie) * 1000
+        #     elif model == "tfidf":
+        #         return self.get_tfidf_value(actor_id, movie_id, tag_of_movie) * 1000
+        #     else:
+        #         exit(1)
+        #
+        # def get_tf_value(self, actor_id, movie_id, tag_of_movie):
+        #     actor_data = self.get_combined_data_for_actor(actor_id)
+        #     doc_data = actor_data[actor_data['movieid'] == movie_id]
+        #     tag_data = doc_data[doc_data['tag'] == tag_of_movie]
+        #     total_tags_count = doc_data.shape[0]
+        #     tag_count = tag_data.shape[0]
+        #     return float(tag_count) / float(total_tags_count)
+        #
+        # def get_idf_value(self, actor_id, tag_of_movie):
+        #     actor_data = self.get_combined_data_for_actor(actor_id)
+        #     movies = actor_data['movieid'].unique()
+        #     doc_count = len(movies)
+        #
+        #     tag_count = 0
+        #     for movie in movies:
+        #         movie_data = actor_data[actor_data['movieid'] == movie]
+        #         unique_tags = movie_data['tag'].unique()
+        #         for tag in unique_tags:
+        #             if tag == tag_of_movie:
+        #                 tag_count += 1
+        #                 break
+        #
+        #     return math.log(float(doc_count) / float(tag_count))
+        #
+        # def get_tfidf_value(self, actor_id, movie_id, tag_of_movie):
+        #     return self.get_tf_value(actor_id, movie_id, tag_of_movie) * self.get_idf_value(actor_id, tag_of_movie)
+        #
+        # def get_epoc_timestamp_for_date(self, timestamp):
+        #     return int(time.mktime(time.strptime(timestamp, "%Y-%m-%d %H:%M:%S")))
+        #
+        # def get_timestamp_value(self, timestamp):
+        #     combined_data = self.get_combined_data()
+        #     timestamps = combined_data['timestamp'].unique()
+        #     all_ts = []
+        #     for ts in timestamps:
+        #         all_ts.append(self.get_epoc_timestamp_for_date(ts))
+        #
+        #     input_ts = self.get_epoc_timestamp_for_date(timestamp)
+        #
+        #     mininum = min(all_ts)
+        #     maximum = max(all_ts)
+        #
+        #     number_of_divisions = 100
+        #     interval = (maximum - mininum) / number_of_divisions
+        #     value = 0
+        #     upper_bound = mininum
+        #     while True:
+        #         if input_ts <= upper_bound:
+        #             break
+        #         upper_bound += interval
+        #         value += 1
+        #
+        #     return value
+        #
+        # def get_actor_rank_value(self, actor_id, movie_id):
+        #     combined_data = self.get_combined_data()
+        #     movie_data = combined_data[combined_data['movieid'] == movie_id]
+        #     ranks = movie_data['actor_movie_rank'].unique()
+        #     mininum = min(ranks)
+        #     maximum = max(ranks)
+        #
+        #     number_of_divisions = 10
+        #     interval = (maximum - mininum) / number_of_divisions
+        #     actor_rank = movie_data[movie_data['actorid'] == actor_id]['actor_movie_rank'].unique()[0]
+        #
+        #     value = 0
+        #     upper_bound = mininum
+        #     while True:
+        #         if actor_rank <= upper_bound:
+        #             break
+        #         upper_bound += interval
+        #         value += 10
+        #
+        #     return 100 - value
+
+
+if __name__ == "__main__":
+    obj = GenreTag()
+    obj.get_combined_data_for_genre("Drama")
+    # print "TF-IDF values for genre : \n"
+    # result = obj.get_weighted_tags_for_genre_and_model(579260, "tfidf")
+    # for key, value in sorted(result.iteritems(), key=lambda (k, v): (v, k), reverse=True):
+    #     print "%s: %s" % (key, value)
