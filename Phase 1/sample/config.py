d1ff1fb799f8bdb492852f6810bd463c538b428b
Aditya <aditya-may9@users.noreply.github.com>
2017-09-16 06:55:40 -0700

    revamping

--- Phase 1/sample/config.py
+++ Phase 1/sample/config.py
@@ -8,20 +8,15 @@ log = logging.getLogger(__name__)
 
 class ParseConfig(object):
     def __init__(self):
-        self.Config = configparser.ConfigParser()
-        self.Config.read("../config.ini")
+        self.config = configparser.ConfigParser()
+        self.config.read("../config.ini")
 
     def config_section_mapper(self, section):
         dict1 = {}
-        options = self.Config.options(section)
+        options = self.config.options(section)
         for option in options:
-            try:
-                dict1[option] = self.Config.get(section, option)
-                if dict1[option] == -1:
-                    log.debug("skip: %s" % option)
-            except:
-                log.error("exception on %s!" % option)
-                dict1[option] = None
+            dict1[option] = self.config.get(section, option)
+
         return dict1
 
 
