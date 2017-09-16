70cd5342322e7c38ee79c282ab09d1052fe2dfec
Aditya <aditya-may9@users.noreply.github.com>
2017-09-16 07:05:23 -0700

    revamping

--- Phase 1/sample/extractor.py
+++ Phase 1/sample/extractor.py
@@ -3,7 +3,7 @@ import os
 
 import pandas as pd
 
-import config
+import config_parser
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -41,7 +41,7 @@ class DataExtractor(object):
 
 
 if __name__ == "__main__":
-    conf = config.ParseConfig()
+    conf = config_parser.ParseConfig()
     data_set_location = conf.config_section_mapper("filePath").get("data_set_location")
     extract_data = ExtractData(data_set_location)
     data_frame = extract_data.data_extractor("mlmovies.csv")
